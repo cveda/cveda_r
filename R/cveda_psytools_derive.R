eba819b9d706697440a407c36732137a888bde78
Dimitri Papadopoulos <dimitri.papadopoulos@cea.fr>
2017-07-18 17:05:45 +0200

    Put all code in a process() function.
    Consistency with Imagen.

--- R/cveda_psytools_derive.R
+++ R/cveda_psytools_derive.R
@@ -47,67 +47,72 @@ escape <- function(x) {
 }
 
 
-# Iterate over exported CSV Psytools files
-for (filename in list.files(PSYTOOLS_PSC2_DIR)) {
-    # The name of the questionnaire is based on the CSV file name
-    name <- file_path_sans_ext(filename)
+process <- function(psc2_dir, processed_dir) {
+    # Iterate over exported CSV Psytools files
+    for (filename in list.files(psc2_dir)) {
+        # The name of the questionnaire is based on the CSV file name
+        name <- file_path_sans_ext(filename)
 
-    # Read each exported CSV Psytools file into a data frame
-    filepath <- file.path(PSYTOOLS_PSC2_DIR, filename)
-    COL_CLASSES = c(
-        "User.code"="character",
-        "Block"="character",
-        "Trial"="character",
-        "Response.time..ms."="numeric")
-    df <- read.csv(filepath, colClasses=COL_CLASSES, stringsAsFactors=FALSE)
+        # Read each exported CSV Psytools file into a data frame
+        filepath <- file.path(psc2_dir, filename)
+        COL_CLASSES = c(
+            "User.code"="character",
+            "Block"="character",
+            "Trial"="character",
+            "Response.time..ms."="numeric")
+        df <- read.csv(filepath, colClasses=COL_CLASSES, stringsAsFactors=FALSE)
 
-    # Discard uncomplete trials
-    df <- subset(df, Completed=='t')
-    # Get rid of Demo, MOCK, NPPILOT and TEST user codes (PSC1-only)
-    df <- subset(df, !grepl("Demo|MOCK|NPPILOT|TEST", User.code, ignore.case=TRUE))
+        # Discard uncomplete trials
+        df <- subset(df, Completed=='t')
+        # Get rid of Demo, MOCK, NPPILOT and TEST user codes (PSC1-only)
+        df <- subset(df, !grepl("Demo|MOCK|NPPILOT|TEST", User.code, ignore.case=TRUE))
 
-    # Add an index to preserve order (to simplify eyeballing)
-    df$rowIndex <- seq_len(nrow(df))
+        # Add an index to preserve order (to simplify eyeballing)
+        df$rowIndex <- seq_len(nrow(df))
 
-    # Apply relevant derivation function to each questionnaire
-    if (name == "cVEDA-cVEDA_SOCRATIS-BASIC_DIGEST") {
-        df <- deriveSOCRATIS(df)
-    } else if (name == "cVEDA-cVEDA_SST-BASIC_DIGEST") {
-        df <- deriveSST(df)
-    } else if (name == "cVEDA-cVEDA_KIRBY-BASIC_DIGEST") {
-        df <- deriveKIRBY(df)
-    } else if (name == "cVEDA-cVEDA_BART-BASIC_DIGEST") {
-        df <- deriveBART(df)
-    } else if (name == "cVEDA-cVEDA_ERT-BASIC_DIGEST") {
-        df <- deriveERT(df)
-    } else if (name == "cVEDA-cVEDA_MID-BASIC_DIGEST") {
-        df <- deriveMID(df)
-    } else if (name == "cVEDA-cVEDA_TMT-TMT_DIGEST") {
-        df <- deriveTMT(df)
-    } else if (name == "cVEDA-cVEDA_WCST-BASIC_DIGEST") {
-        df <- deriveWCST(df)
-    } else if (name == "cVEDA-cVEDA_CORSI-BASIC_DIGEST") {
-        df <- deriveCORSI(df)
-    } else if (name == "cVEDA-cVEDA_DS-BASIC_DIGEST") {
-        df <- deriveDS(df)
-    } else {
-        df <- rotateQuestionnaire(df)
+        # Apply relevant derivation function to each questionnaire
+        if (name == "cVEDA-cVEDA_SOCRATIS-BASIC_DIGEST") {
+            df <- deriveSOCRATIS(df)
+        } else if (name == "cVEDA-cVEDA_SST-BASIC_DIGEST") {
+            df <- deriveSST(df)
+        } else if (name == "cVEDA-cVEDA_KIRBY-BASIC_DIGEST") {
+            df <- deriveKIRBY(df)
+        } else if (name == "cVEDA-cVEDA_BART-BASIC_DIGEST") {
+            df <- deriveBART(df)
+        } else if (name == "cVEDA-cVEDA_ERT-BASIC_DIGEST") {
+            df <- deriveERT(df)
+        } else if (name == "cVEDA-cVEDA_MID-BASIC_DIGEST") {
+            df <- deriveMID(df)
+        } else if (name == "cVEDA-cVEDA_TMT-TMT_DIGEST") {
+            df <- deriveTMT(df)
+        } else if (name == "cVEDA-cVEDA_WCST-BASIC_DIGEST") {
+            df <- deriveWCST(df)
+        } else if (name == "cVEDA-cVEDA_CORSI-BASIC_DIGEST") {
+            df <- deriveCORSI(df)
+        } else if (name == "cVEDA-cVEDA_DS-BASIC_DIGEST") {
+            df <- deriveDS(df)
+        } else {
+            df <- rotateQuestionnaire(df)
+        }
+
+        # Extract "Age.band" from "User.code"
+        df$Age.band <- as.numeric(substr(df$User.code, 15, 15))
+        df$User.code <- substr(df$User.code, 1, 12)
+        df <- df[c(1, ncol(df), 2:(ncol(df)-1))]
+
+        # Roll our own quoting method
+        for (column in colnames(df)) {
+            df[,column] <- escape(df[,column])
+        }
+
+        # Write data frame back to the processed CSV file
+        filepath <- file.path(processed_dir, filename)
+        columns <- sub("\\.ms\\.", "[ms]", colnames(df))  # Response time [ms]
+        columns <- gsub("\\.", " ", columns)
+        write.table(df, filepath, quote=FALSE, sep=",", na="",
+                    row.names=FALSE, col.names=columns)
     }
-
-    # Extract "Age.band" from "User.code"
-    df$Age.band <- as.numeric(substr(df$User.code, 15, 15))
-    df$User.code <- substr(df$User.code, 1, 12)
-    df <- df[c(1, ncol(df), 2:(ncol(df)-1))]
-
-    # Roll our own quoting method
-    for (column in colnames(df)) {
-        df[,column] <- escape(df[,column])
-    }
-
-    # Write data frame back to the processed CSV file
-    filepath <- file.path(PSYTOOLS_PROCESSED_DIR, filename)
-    columns <- sub("\\.ms\\.", "[ms]", colnames(df))  # Response time [ms]
-    columns <- gsub("\\.", " ", columns)
-    write.table(df, filepath, quote=FALSE, sep=",", na="",
-                row.names=FALSE, col.names=columns)
 }
+
+
+process(PSYTOOLS_PSC2_DIR, PSYTOOLS_PROCESSED_DIR)
