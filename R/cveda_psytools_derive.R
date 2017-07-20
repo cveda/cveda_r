0e550bdd91962ba7f031b808f35d266c954406e9
Dimitri Papadopoulos <dimitri.papadopoulos@cea.fr>
2017-07-20 09:00:33 +0200

    df -> d
    df is already a base function name, do not reuse it.
    
    https://stackoverflow.com/questions/6135868/in-r-what-exactly-is-the-problem-with-having-variables-with-the-same-name-as-ba
    https://stackoverflow.com/questions/6979630/what-1-2-letter-object-names-conflict-with-existing-r-objects

--- R/cveda_psytools_derive.R
+++ R/cveda_psytools_derive.R
@@ -60,56 +60,56 @@ process <- function(psc2_dir, processed_dir) {
             "Block"="character",
             "Trial"="character",
             "Response.time..ms."="numeric")
-        df <- read.csv(filepath, colClasses=COL_CLASSES, stringsAsFactors=FALSE)
+        d <- read.csv(filepath, colClasses=COL_CLASSES, stringsAsFactors=FALSE)
 
         # Discard uncomplete trials
-        df <- subset(df, Completed=='t')
+        d <- subset(d, Completed=='t')
         # Get rid of Demo, MOCK, NPPILOT and TEST user codes (PSC1-only)
-        df <- subset(df, !grepl("Demo|MOCK|NPPILOT|TEST", User.code, ignore.case=TRUE))
+        d <- subset(d, !grepl("Demo|MOCK|NPPILOT|TEST", User.code, ignore.case=TRUE))
 
         # Add an index to preserve order (to simplify eyeballing)
-        df$rowIndex <- seq_len(nrow(df))
+        d$rowIndex <- seq_len(nrow(d))
 
         # Apply relevant derivation function to each questionnaire
         if (name == "cVEDA-cVEDA_SOCRATIS-BASIC_DIGEST") {
-            df <- deriveSOCRATIS(df)
+            d <- deriveSOCRATIS(d)
         } else if (name == "cVEDA-cVEDA_SST-BASIC_DIGEST") {
-            df <- deriveSST(df)
+            d <- deriveSST(d)
         } else if (name == "cVEDA-cVEDA_KIRBY-BASIC_DIGEST") {
-            df <- deriveKIRBY(df)
+            d <- deriveKIRBY(d)
         } else if (name == "cVEDA-cVEDA_BART-BASIC_DIGEST") {
-            df <- deriveBART(df)
+            d <- deriveBART(d)
         } else if (name == "cVEDA-cVEDA_ERT-BASIC_DIGEST") {
-            df <- deriveERT(df)
+            d <- deriveERT(d)
         } else if (name == "cVEDA-cVEDA_MID-BASIC_DIGEST") {
-            df <- deriveMID(df)
+            d <- deriveMID(d)
         } else if (name == "cVEDA-cVEDA_TMT-TMT_DIGEST") {
-            df <- deriveTMT(df)
+            d <- deriveTMT(d)
         } else if (name == "cVEDA-cVEDA_WCST-BASIC_DIGEST") {
-            df <- deriveWCST(df)
+            d <- deriveWCST(d)
         } else if (name == "cVEDA-cVEDA_CORSI-BASIC_DIGEST") {
-            df <- deriveCORSI(df)
+            d <- deriveCORSI(d)
         } else if (name == "cVEDA-cVEDA_DS-BASIC_DIGEST") {
-            df <- deriveDS(df)
+            d <- deriveDS(d)
         } else {
-            df <- rotateQuestionnaire(df)
+            d <- rotateQuestionnaire(d)
         }
 
         # Extract "Age.band" from "User.code"
-        df$Age.band <- as.numeric(substr(df$User.code, 15, 15))
-        df$User.code <- substr(df$User.code, 1, 12)
-        df <- df[c(1, ncol(df), 2:(ncol(df)-1))]
+        d$Age.band <- as.numeric(substr(d$User.code, 15, 15))
+        d$User.code <- substr(d$User.code, 1, 12)
+        d <- d[c(1, ncol(d), 2:(ncol(d)-1))]
 
         # Roll our own quoting method
-        for (column in colnames(df)) {
-            df[,column] <- escape(df[,column])
+        for (column in colnames(d)) {
+            d[,column] <- escape(d[,column])
         }
 
         # Write data frame back to the processed CSV file
         filepath <- file.path(processed_dir, filename)
-        columns <- sub("\\.ms\\.", "[ms]", colnames(df))  # Response time [ms]
+        columns <- sub("\\.ms\\.", "[ms]", colnames(d))  # Response time [ms]
         columns <- gsub("\\.", " ", columns)
-        write.table(df, filepath, quote=FALSE, sep=",", na="",
+        write.table(d, filepath, quote=FALSE, sep=",", na="",
                     row.names=FALSE, col.names=columns)
     }
 }
